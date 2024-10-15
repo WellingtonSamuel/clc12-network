fadbd8f3b99066816364c291b938da1f37f3c448
EC2 Default User <ec2-user@ip-172-31-63-93.ec2.internal>
2024-10-15 22:51:14 +0000

    modificado backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "clc12-network-wellington"
+    bucket = "clc12-network-wellington2"
     key    = "network/terraform.tfstate"
     region = "us-east-1"
   }
